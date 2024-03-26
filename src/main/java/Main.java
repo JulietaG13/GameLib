a8be9f3ad57d857dfb3b4115d229120feb8114cf
JulietaG13 <jgarberoglio@mail.austral.edu.ar>
2024-03-26 10:42:35 -0300

    changed example

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,36 +1,49 @@
-import model.Publication;
-import model.Rol;
 import model.User;
+import persistence.Database;
+import repository.Users;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.time.LocalDateTime;
-import java.util.Collections;
 import java.util.List;
+
+import static spark.Spark.*;
 
 public class Main {
 
     public static void main(String[] args) {
+        new Database().startDBServer();
         final EntityManagerFactory factory = Persistence.createEntityManagerFactory("gamelib");
 
         final EntityManager entityManager = factory.createEntityManager();
 
         sample1(entityManager);
 
+        List<User> users = new Users(entityManager).listAll();
+        StringBuilder str = new StringBuilder();
+        for(User u : users) str.append(u.getUsername()).append(" - ").append(u.getEmail()).append("<br>");
+
+        // http://localhost:4567/hello
+        get("/hello", (req, res) -> str.toString());
+
         entityManager.close();
     }
 
     private static void sample1(EntityManager entityManager) {
         final EntityTransaction transaction = entityManager.getTransaction();
+        Users usersRepo = new Users(entityManager);
 
         transaction.begin();
-        final User user1 = new User("username1", "u1@jedis.org", "12345678");
+        final User user1 = new User("username1", "u1@jedis.org", "qwerty123");
         final User user2 = new User("username2", "u2@jedis.org", "qwerty123");
+        final User user3 = new User("username3", "u3@jedis.org", "qwerty123");
+        final User user4 = new User("username4", "u4@jedis.org", "qwerty123");
 
-        entityManager.persist(user1);
-        entityManager.persist(user2);
+        usersRepo.persist(user1);
+        usersRepo.persist(user2);
+        usersRepo.persist(user3);
+        usersRepo.persist(user4);
 
         transaction.commit();
     }
