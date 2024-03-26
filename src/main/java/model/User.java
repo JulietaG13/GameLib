28ee768a6b705935e51f3ae74af99ac362ce3e5c
JulietaG13 <jgarberoglio@mail.austral.edu.ar>
2024-03-26 10:42:08 -0300

    added getters/setters

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -48,4 +48,52 @@ public class User {
     public User(String username, String email, String password) {
         this(username, email, password, Rol.USER);
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
 }
