3c6292812e949d11fa6666d72cb3b0c93591b3d0
Agustin <almartinez1@mail.austral.edu.ar>
2024-04-09 13:16:43 -0300

    login + signup data send impl

--- src/main/front/gamelib/src/Components/LoginSignup/Login.jsx
+++ src/main/front/gamelib/src/Components/LoginSignup/Login.jsx
@@ -3,41 +3,66 @@ import './LoginSignup.css'
 
 import email_icon from '../Assets/mail-icon.png'
 import password_icon from '../Assets/password icon.png'
-import user_icon from '../Assets/user-icon.png'
-import {Link} from "react-router-dom";
+    import {Link, Navigate} from "react-router-dom";
+import axios from "axios";
 
 
-const LoginSignup = () => {
+const Login = () => {
     //React code, provides a variable and a set to provide the variable data
-    const [action,setAction] = useState("Sign Up");
+    const [mail, setMail] = useState('');
+    const [password, setPassword] = useState('');
+    const [navigate, setNavigate] = useState(false);
 
+    const submit = async e => {
+        e.preventDefault();
+        const response = await axios.post('http://localhost:8080/api/login', {
+            mail: mail, password: password
+        }, {withCredentials: true});
+
+        console.log(response.data)
+
+
+        //setNavigate(true);
+    }
+
+    if (navigate) {
+        return <Navigate to={"/"}/>
+    }
 
     return(
-        <div className='container'>
-            <div className='header'>
-                <div className="text">{"Login"}</div>
-                <div className="underline"></div>
-            </div>
-            <div className="inputs">
-                <div className="input">
-                    <img src={email_icon} alt=""/>
-                    <input type="email" placeholder={"Email"}/>
+        <form onSubmit={submit}>
+            <div className='container'>
+                <div className='header'>
+                    <div className="text">{"Login"}</div>
+                    <div className="underline"></div>
                 </div>
-                <div className="input">
-                    <img src={password_icon} alt=""/>
-                    <input type="password" placeholder={"Password"}/>
-                </div>
-            </div>
-            <div className="forgot-password">New here? <Link to="/register" className={"link"}> Register </Link></div>
-            <div className="submit-container">
-                <div className={"submit"}
-                     onClick={() => {}}>Login
-                </div>
-            </div>
+                <div className="inputs">
+                    <div className="input">
+                        <img src={email_icon} alt=""/>
+                        <input type="email" placeholder={"Email"}
+                        onChange={e => setMail(e.target.value)}
 
-        </div>
+                        />
+                    </div>
+                    <div className="input">
+                        <img src={password_icon} alt=""/>
+                        <input type="password" placeholder={"Password"}
+                        onChange={e => setPassword(e.target.value)}
+
+                        />
+                    </div>
+                </div>
+                <div className="forgot-password">New here? <Link to="/register" className={"link"}> Register </Link></div>
+                <div className="submit-container">
+                    <div className={"submit"}
+                         onClick={() => {}}>Login
+                    </div>
+                </div>
+
+            </div>
+        </form>
     );
 };
-export default LoginSignup;
+export default Login;
 
 
--- src/main/front/gamelib/src/Components/LoginSignup/register.jsx
+++ src/main/front/gamelib/src/Components/LoginSignup/register.jsx
@@ -1,14 +1,34 @@
 import React, {useState} from "react"
 import './LoginSignup.css'
+import axios from "axios";
 
 import email_icon from '../Assets/mail-icon.png'
 import password_icon from '../Assets/password icon.png'
 import user_icon from '../Assets/user-icon.png'
 import {Link} from "react-router-dom";
+import {Navigate} from "react-router-dom";
 
-const Register = () => {
-    //React code, provides a variable and a set to provide the variable data
+export const Register = () => {
+
+    const [name, setName] = useState('');
+    const [mail, setMail] = useState('');
+    const [password, setPassword] = useState('');
+    const [navigate, setNavigate] = useState(false);
+
+    const submit = async e => {
+        e.preventDefault();
+        await axios.post('http://localhost:8080/api/register', {
+            name: name, mail: mail, password: password
+        });
+        setNavigate(true);
+    }
+
+    if (navigate) {
+        return <Navigate to={"/login"}/>
+    }
+
     return(
+    <form onSubmit={submit}>
         <div className='container'>
             <div className='header'>
                 <div className="text">Sign up</div>
@@ -17,15 +37,21 @@ const Register = () => {
             <div className="inputs">
                 <div className="input">
                     <img src={user_icon} alt=""/>
-                    <input type="text" placeholder={"Name"}/>
+                    <input type="text" placeholder={"Name"}
+                           onChange={e => setName(e.target.value)}
+                    />
                 </div>
                 <div className="input">
                     <img src={email_icon} alt=""/>
-                    <input type="email" placeholder={"Email"}/>
+                    <input type="email" placeholder={"Email"}
+                           onChange={e => setMail(e.target.value)}
+                    />
                 </div>
                 <div className="input">
                     <img src={password_icon} alt=""/>
-                    <input type="password" placeholder={"Password"}/>
+                    <input type="password" placeholder={"Password"}
+                           onChange={e => setPassword(e.target.value)}
+                    />
                 </div>
             </div>
             <div className="forgot-password">Do you already have an account? <Link to={"/login"} className={"link"}>Log in</Link></div>
@@ -35,7 +61,8 @@ const Register = () => {
                 </div>
             </div>
         </div>
-    );
+    </form>
+);
 };
 export default Register;
 
