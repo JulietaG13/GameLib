704113a6be830593480e6c6b24da4d2714823a47
Agustin <almartinez1@mail.austral.edu.ar>
2024-04-20 16:44:51 -0300

    Added login/signup message when they fail

--- src/main/front/gamelib/src/App.js
+++ src/main/front/gamelib/src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
-import Header from "./Components/Header/Header"
 import {BrowserRouter, Routes, Route, Outlet} from "react-router-dom"
 import Login from "./Components/LoginSignup/Login";
 import {Register} from "./Components/LoginSignup/register";
 import ABM from "./Components/ManageVideogame/ManageVideogame";
 import Home from "./Components/Home/Home";
+import Header from "./Components/Header/Header";
 
 function App() {
     return <BrowserRouter>
-        <Header/>
+        <Header></Header>
         <Routes>
-            <Route path="/" element={<Home/>}></Route>
+            <Route path="/" element={<Home/>}/>
             <Route path="/Login" element={<Login/>}/>
             <Route path="/register" element={<Register/>}/>
             <Route path="/addVideogame" element={<ABM type={"Add"} id={null} />} />
@@ -18,10 +18,5 @@ function App() {
         </Routes>
     </BrowserRouter>
 }
-//return (
-//    <div>
-//        <Header/>
-//        <Register/>
-//    </div>
-//);
+
 export default App;
