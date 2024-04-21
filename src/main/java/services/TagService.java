aad9b545da382cef833e84331055d8275e0c0b1d
JuliG13 <128094821+JulietaG13@users.noreply.github.com>
2024-04-21 11:49:47 -0300

    Implemented TagService for game tags

---
+++ src/main/java/services/TagService.java
@@ -0,0 +1,41 @@
+package services;
+
+import model.Tag;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import java.util.List;
+import java.util.Optional;
+
+public class TagService {
+  
+  private final EntityManager entityManager;
+  
+  public TagService(EntityManager entityManager) {
+    this.entityManager = entityManager;
+  }
+  
+  public Optional<Tag> findById(Long id) {
+    return Optional.ofNullable(entityManager.find(Tag.class, id));
+  }
+  
+  public Optional<Tag> findByName(String name) {
+    return entityManager
+        .createQuery("SELECT t FROM Tag t WHERE t.name LIKE :name", Tag.class)
+        .setParameter("name", name).getResultList()
+        .stream()
+        .findFirst();
+  }
+  
+  public List<Tag> listAll() {
+    return entityManager.createQuery("SELECT t FROM Tag t", Tag.class).getResultList();
+  }
+  
+  public Tag persist(Tag tag) {
+    EntityTransaction tx = entityManager.getTransaction();
+    tx.begin();
+    entityManager.persist(tag);
+    tx.commit();
+    return tag;
+  }
+}
