6cc35a99eb9663f981b3d3dd9d723bcbc133a56c
JulietaG13 <jgarberoglio@mail.austral.edu.ar>
2024-05-25 00:27:39 -0300

    Implement route to get a user's shelves

--- src/main/java/controllers/ShelfController.java
+++ src/main/java/controllers/ShelfController.java
@@ -1,17 +1,22 @@
 package controllers;
 
 import com.google.gson.JsonArray;
+import entities.ErrorMessages;
+import interfaces.Controller;
 import model.Shelf;
+import model.User;
 import services.ShelfService;
+import services.UserService;
 import spark.Spark;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import java.util.List;
+import java.util.Optional;
 
-public class ShelfController {
+public class ShelfController implements Controller {
     private static final String ROUTE_GET_ALL = "/shelf/all";
-    private static final String ROUTE_GET_FROM_USER = "/shelf/:username/:max";
+    private static final String ROUTE_GET_FROM_USER = "/shelf/get/:username/:max";
 
     private EntityManagerFactory factory;
     private static ShelfController instance;
@@ -27,8 +32,9 @@ public class ShelfController {
         return instance;
     }
 
-    public void routes() {
+    public void run() {
         routeGetAll();
+        routeGetFromUser();
     }
 
     private void routeGetAll() {
@@ -42,6 +48,49 @@ public class ShelfController {
 
             JsonArray array = new JsonArray();
             allShelves.forEach(s -> array.add(s.asJson()));
+
+            em.close();
+            return array.toString();
+        });
+    }
+
+    private void routeGetFromUser() {
+        Spark.get(ROUTE_GET_FROM_USER, "application/json", (req, resp) -> { // :username, :max
+            EntityManager em = factory.createEntityManager();
+
+            int max;
+            try {
+                String maxStr = req.params(":max");
+                if (maxStr == null || maxStr.isEmpty()) {
+                    max = 10;
+                } else {
+                    max = Integer.parseInt(maxStr);
+                }
+            } catch (NumberFormatException e) {
+                resp.status(403);
+                return "Max number of games must be a number!";
+            }
+
+            String username = req.params(":username");
+            if (username == null || username.isEmpty()) {
+                resp.status(403);
+                return "You must provide a username!";
+            }
+            UserService userService = new UserService(em);
+            Optional<User> owner = userService.findByUsername(username);
+            if (owner.isEmpty()) {
+                resp.status(404);
+                return ErrorMessages.usernameNotFound(username);
+            }
+
+            ShelfService shelfService = new ShelfService(em);
+            List<Shelf> shelves = shelfService.listByUser(owner.get());
+
+            resp.status(200);
+            resp.type("application/json");
+
+            JsonArray array = new JsonArray();
+            shelves.forEach(s -> array.add(s.asJson()));
 
             em.close();
             return array.toString();
