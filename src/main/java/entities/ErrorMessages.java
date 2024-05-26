b2304d166815fc20d8dcfe59d0a75c6ee04ad756
JuliG13 <128094821+JulietaG13@users.noreply.github.com>
2024-05-26 01:38:31 -0300

    Added standard error messages

--- src/main/java/entities/ErrorMessages.java
+++ src/main/java/entities/ErrorMessages.java
@@ -5,4 +5,16 @@ public class ErrorMessages {
     public static String usernameNotFound(String username) {
         return "There is no user " + username + "!";
     }
+    
+    public static String informationNotProvided(String info) {
+        return "You must provide a " + info + "!";
+    }
+    
+    public static String informationNotFound(String info) {
+        return info + " not found!";
+    }
+    
+    public static String informationNotNumber(String info) {
+        return info + " must be a number!";
+    }
 }
