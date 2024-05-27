00967e1b0d5455e5e57f2bacda1674fc7f610746
Agustin <almartinez1@mail.austral.edu.ar>
2024-05-27 19:33:15 -0300

    logo rework, library button in header removed

--- src/main/front/gamelib/src/Components/Header/HeaderV2.jsx
+++ src/main/front/gamelib/src/Components/Header/HeaderV2.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useRef, useState} from "react";
-import gamelibLogo from '../Assets/gamelib-logo.jpg';
+import gameLibLogoRework from '../Assets/gamelibLogoReRework.png';
 import user_icon from "../Assets/user-icon.png";
 import axios from "axios";
 import {Link} from "react-router-dom";
@@ -81,30 +81,27 @@ function HeaderV2() {
     return (
         <div className={'flex items-center bg-[#ff8341] h-20'}>
             <a href={'http://localhost:3000/'}>
-                <img src={gamelibLogo} width={60} height={60} className={'ml-3'} alt={""}/>
+                <img src={gameLibLogoRework} width={80} height={80} className={'ml-3'} alt={""}/>
             </a>
             <div className={'flex bg-slate-300 p-2 w-full h-12 items-center ml-10 mr-20 rounded-full'}>
                 <input type={'text'} placeholder={'Search Games'} className={'bg-transparent outline-none '}/>
             </div>
             <div className={'mr-48'}>
-                <h2 className={'font-bold text-black'}>
-                    <a href={'http://localhost:3000/library'} className={'text-[25px]'}>Library</a>
-                </h2>
             </div>
             {isLoggedIn ? (
-                <div className="flex flex-row justify-end p-1 items-center relative"  ref={dropdownRef}>
-                    <h2>{localStorage.getItem('username') || 'Name'}</h2>
-                    <img src={user_icon} alt={"user icon"} className={'cursor-pointer w-[5em] h-[4-em]'}
-                         onClick={toggleDropdown}/>
-                    {showDropdown && (
-                            <div className="dropdown-content absolute bg-gray-100 w-40 py-2 shadow-md z-10 top-full right-10 flex flex-col pl-2 rounded-b-xl rounded-t-md">
-                            <Link to={`/profile/${localStorage.getItem("username")}`}>Profile</Link>
-                            <a href="#" onClick={handleLogout}>Logout</a>
-                            <a onClick={handleDeleteUser}>Delete account</a>
-                        </div>
-                    )}
+            <div className="flex flex-row justify-end p-1 items-center relative"  ref={dropdownRef}>
+                <h2>{localStorage.getItem('username') || 'Name'}</h2>
+                <img src={user_icon} alt={"user icon"} className={'cursor-pointer w-[5em] h-[4-em]'}
+                     onClick={toggleDropdown}/>
+                {showDropdown && (
+                        <div className="dropdown-content absolute bg-gray-100 w-40 py-2 shadow-md z-10 top-full right-10 flex flex-col pl-2 rounded-b-xl rounded-t-md">
+                        <Link to={`/profile/${localStorage.getItem("username")}`}>Profile</Link>
+                        <a href="#" onClick={handleLogout}>Logout</a>
+                        <a onClick={handleDeleteUser}>Delete account</a>
+                    </div>
+                )}
 
-                </div>
+            </div>
 
             ) : (
                 <h2 className={"font-bold mr-10 text-[20px]"}>
