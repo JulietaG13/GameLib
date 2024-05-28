d4ee8622de35acf2e2fd0e792f1d86d41e7c3ba7
JuliG13 <128094821+JulietaG13@users.noreply.github.com>
2024-05-28 00:26:55 -0300

    Implement methods to retrieve News

---
+++ src/main/java/repositories/NewsRepository.java
@@ -0,0 +1,74 @@
+package repositories;
+
+import model.Game;
+import model.News;
+import model.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import java.util.List;
+import java.util.Optional;
+
+public class NewsRepository {
+  
+  private final EntityManager entityManager;
+  
+  public NewsRepository(EntityManager entityManager) {
+    this.entityManager = entityManager;
+  }
+  
+  public Optional<News> findById(Long id) {
+    return Optional.ofNullable(entityManager.find(News.class, id));
+  }
+  
+  public Optional<News> findByTitle(String title) {
+    return entityManager
+        .createQuery("SELECT n FROM News n WHERE n.title LIKE :title ORDER BY n.date DESC", News.class)
+        .setParameter("title", title)
+        .getResultList()
+        .stream()
+        .findFirst();
+  }
+  
+  public List<News> findByGame(Game game, int max) {
+    return entityManager
+        .createQuery("SELECT n FROM News n WHERE n.game = :game ORDER BY n.date DESC", News.class)
+        .setParameter("game", game)
+        .setMaxResults(max)
+        .getResultList();
+  }
+  
+  public List<News> findByGame(Game game) {
+    return entityManager
+        .createQuery("SELECT n FROM News n WHERE n.game = :game ORDER BY n.date DESC", News.class)
+        .setParameter("game", game)
+        .getResultList();
+  }
+  
+  public List<News> findByAuthor(User author, int max) {
+    return entityManager
+        .createQuery("SELECT n FROM News n WHERE n.author = :author ORDER BY n.date DESC", News.class)
+        .setParameter("author", author)
+        .setMaxResults(max)
+        .getResultList();
+  }
+  
+  public List<News> findByAuthor(User author) {
+    return entityManager
+        .createQuery("SELECT n FROM News n WHERE n.author = :author ORDER BY n.date DESC", News.class)
+        .setParameter("author", author)
+        .getResultList();
+  }
+  
+  public List<News> listAll() {
+    return entityManager.createQuery("SELECT n FROM News n ORDER BY n.date DESC", News.class).getResultList();
+  }
+  
+  public News persist(News news) {
+    EntityTransaction tx = entityManager.getTransaction();
+    tx.begin();
+    entityManager.persist(news);
+    tx.commit();
+    return news;
+  }
+}
