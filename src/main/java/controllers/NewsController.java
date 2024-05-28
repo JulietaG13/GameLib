749672418e33e16d4636b19880b77b3d43332174
JulietaG13 <jgarberoglio@mail.austral.edu.ar>
2024-05-28 10:27:13 -0300

    Implement route to add News to Game

--- src/main/java/controllers/NewsController.java
+++ src/main/java/controllers/NewsController.java
@@ -1,12 +1,18 @@
 package controllers;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import entities.ErrorMessages;
+import entities.Token;
 import interfaces.Controller;
 import model.Game;
 import model.News;
+import model.User;
 import repositories.GameRepository;
 import repositories.NewsRepository;
+import repositories.UserRepository;
+import services.AccessControlService;
 import spark.Spark;
 
 import javax.persistence.EntityManager;
@@ -17,6 +23,7 @@ import java.util.Optional;
 public class NewsController implements Controller {
   
   private static final String ROUTE_GET_FROM_GAME = "/news/get/game/:game_id";
+  private static final String ROUTE_ADD_TO_GAME = "/news/add/game/:game_id";
   
   private EntityManagerFactory factory;
   private static Controller instance;
@@ -35,6 +42,7 @@ public class NewsController implements Controller {
   @Override
   public void run() {
     routeGetFromGame();
+    routeAddToGame();
   }
   
   private void routeGetFromGame() {
@@ -68,6 +76,61 @@ public class NewsController implements Controller {
       
       em.close();
       return jsonArray;
+    });
+  }
+
+  private void routeAddToGame() {
+    Spark.post(ROUTE_ADD_TO_GAME, "application/json", (req, resp) -> {
+      // :game_id | body: title,description | header: token
+      EntityManager em = factory.createEntityManager();
+
+
+      String token = req.headers(Token.PROPERTY_NAME);
+      if (token == null || !AccessControlService.isTokenValid(token)) {
+        resp.status(401);
+        return ErrorMessages.userMustBeLoggedIn();
+      }
+
+      String username = AccessControlService.getUsernameFromToken(token);
+      UserRepository userRepository = new UserRepository(em);
+      Optional<User> user = userRepository.findByUsername(username);
+      if (user.isEmpty()) {
+        resp.status(404);
+        return ErrorMessages.usernameNotFound(username);
+      }
+
+      String gameId = req.params(":game_id");
+      GameRepository gameRepository = new GameRepository(em);
+      Optional<Game> game = gameRepository.findById(Long.parseLong(gameId));
+      if (game.isEmpty()) {
+        resp.status(404);
+        return ErrorMessages.informationNotFound("Game");
+      }
+
+      JsonObject body = JsonParser
+              .parseString(req.body())
+              .getAsJsonObject();
+
+      String title = body.get("title").getAsString();
+      if (title == null || title.isEmpty()) {
+        resp.status(400);
+        return ErrorMessages.informationNotProvided("Title");
+      }
+
+      String description = body.get("description").getAsString();
+      if (description == null || description.isEmpty()) {
+        resp.status(400);
+        return ErrorMessages.informationNotProvided("Description");
+      }
+
+      NewsRepository newsRepository = new NewsRepository(em);
+      News news = new News(title, description, game.get(), user.get());
+      newsRepository.persist(news);
+
+      // TODO(notify users of new news :) !)
+
+      em.close();
+      return news.asJson();
     });
   }
 }
