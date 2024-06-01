c8d0d83a6bb4cfcf2b8f05c2570d68cf80b1159d
JulietaG13 <jgarberoglio@mail.austral.edu.ar>
2024-06-01 01:40:36 -0300

    Implement GameService to check if user is able to edit an specific game

---
+++ src/main/java/services/GameService.java
@@ -0,0 +1,12 @@
+package services;
+
+import entities.Rol;
+import model.Game;
+import model.User;
+
+public class GameService {
+
+    public static boolean isAbleToEditGame(User user, Game game) {
+        return user.getRol() == Rol.ADMIN || user.getId().equals(game.getOwner().getId());
+    }
+}
