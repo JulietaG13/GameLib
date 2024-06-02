723bdedc13ca069d52975d852c48bd3f4110ed5d
JuliG13 <128094821+JulietaG13@users.noreply.github.com>
2024-06-02 18:08:38 -0300

    Implement notifyUsers for game subscribers

--- src/main/java/services/NewsService.java
+++ src/main/java/services/NewsService.java
@@ -1,11 +1,25 @@
 package services;
 
 import entities.Rol;
+import model.Game;
 import model.News;
 import model.User;
 
+import java.util.Set;
+
 public class NewsService {
+    
     public static boolean isAbleToDeleteNews(User user, News news) {
         return user.getRol() == Rol.ADMIN || user.getId().equals(news.getAuthor().getId());
+    }
+    
+    public static void notifyUsers(News news) {
+        //User dev = news.getAuthor();  TODO()
+        Game game = news.getGame();
+    
+        Set<User> gameSubs = game.getSubscribers();
+        if (!gameSubs.isEmpty()) {
+            EmailService.sendMail(EmailService.getEmails(gameSubs), news);
+        }
     }
 }
