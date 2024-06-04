11e4825f70ac27ac471a0fee56de174734bdfb79
JulietaG13 <jgarberoglio@mail.austral.edu.ar>
2024-06-04 13:02:17 -0300

    Added a spinner while loading the Library page

--- src/main/front/gamelib/src/Components/TheLibrary/Library.jsx
+++ src/main/front/gamelib/src/Components/TheLibrary/Library.jsx
@@ -6,25 +6,40 @@ import GamesByGenresId from './GamesByGenresId'
 import GamesFromDB from './GamesFromDB'
 import axios from "axios";
 import HeaderV2 from "../Header/HeaderV2";
+import Loader from "./Loader";
 
 
 function Library() {
     const [gamesFromDB, setGamesFromDB] = useState([]);
+    const [isLoading, setIsLoading] = useState(false)
 
     useEffect(() => {
         getGamesFromDB()
     }, []);
 
     const getGamesFromDB = () => {
+        setIsLoading(true)
         axios.get('http://localhost:4567/games').then((response) => {
             console.log("gamesFromDB:", response)
             setGamesFromDB(response.data)
+            setIsLoading(false)
         });
     }
 
     return(
         <div>
             <HeaderV2/>
+            {isLoading ? (
+                <div
+                    style={{
+                        width: "100px",
+                        margin: "auto",
+                        marginTop: "20px"
+                    }}
+                >
+                    <Loader/>
+                </div>
+            ) : null }
             <div className='grid grid-cols-4 p-5 bg-gray-200  '>
                 <div className='h-full hidden md:block'>
                     {/*<GenreList genreId={(genreId) => getGameListByGenreId(genreId)}/>*/}
