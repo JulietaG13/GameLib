a184cadaff552c970d72df335bfccfaeedc8c650
Agustin <almartinez1@mail.austral.edu.ar>
2024-06-08 16:01:49 -0300

    changed header style

--- src/main/front/gamelib/src/Components/Header/HeaderV2.jsx
+++ src/main/front/gamelib/src/Components/Header/HeaderV2.jsx
@@ -78,38 +78,37 @@ function HeaderV2() {
         })
     }
 
+
     return (
-        <div className={'flex items-center bg-[#ff8341] h-20'}>
-            <a href={'http://localhost:3000/'}>
-                <img src={gameLibLogoRework} width={80} height={80} className={'ml-3'} alt={""}/>
+        <div className="flex items-center bg-[#ff8341] h-20 px-4 justify-between">
+            <a href="http://localhost:3000/">
+                <img src={gameLibLogoRework} width={80} height={80} className="ml-3 min-w-[50px]" alt="" />
             </a>
-            <div className={'flex bg-slate-300 p-2 w-full h-12 items-center ml-10 mr-20 rounded-full'}>
-                <input type={'text'} placeholder={'Search Games'} className={'bg-transparent outline-none '}/>
+            <div className="flex bg-slate-300 p-2 w-full h-12 items-center mx-4 rounded-full max-w-[600px] flex-grow">
+                <input type="text" placeholder="Search Games" className="bg-transparent outline-none w-full" />
             </div>
-            <div className={'mr-48'}>
-            </div>
-            {isLoggedIn ? (
-            <div className="flex flex-row justify-end p-1 items-center relative"  ref={dropdownRef}>
-                <h2>{localStorage.getItem('username') || 'Name'}</h2>
-                <img src={user_icon} alt={"user icon"} className={'cursor-pointer w-[5em] h-[4-em]'}
-                     onClick={toggleDropdown}/>
-                {showDropdown && (
-                        <div className="dropdown-content absolute bg-gray-100 w-40 py-2 shadow-md z-10 top-full right-10 flex flex-col pl-2 rounded-b-xl rounded-t-md">
-                        <Link to={`/profile/${localStorage.getItem("username")}`}>Profile</Link>
-                        <a href="#" onClick={handleLogout}>Logout</a>
-                        <a onClick={handleDeleteUser}>Delete account</a>
+            <div className="flex items-center">
+                {isLoggedIn ? (
+                    <div className="flex flex-row justify-end p-1 items-center relative" ref={dropdownRef}>
+                        <h2 className="mr-2">{localStorage.getItem('username') || 'Name'}</h2>
+                        <img src={user_icon} alt="user icon" className="cursor-pointer w-[3em] h-[3em]" onClick={toggleDropdown} />
+                        {showDropdown && (
+                            <div className="dropdown-content absolute bg-gray-100 w-40 py-2 shadow-md z-10 top-full right-0 flex flex-col pl-2 rounded-b-xl rounded-t-md">
+                                <Link to={`/profile/${localStorage.getItem("username")}`}>Profile</Link>
+                                <a href="#" onClick={handleLogout}>Logout</a>
+                                <a onClick={handleDeleteUser}>Delete account</a>
+                            </div>
+                        )}
                     </div>
+                ) : (
+                    <h2 className="font-bold text-[20px]">
+                        <a href="/login">Login</a>
+                    </h2>
                 )}
-
             </div>
-
-            ) : (
-                <h2 className={"font-bold mr-10 text-[20px]"}>
-                    <a href="/login">Login</a>
-                </h2>
-            )}
         </div>
-    )
+    );
+
 }
 
 export default HeaderV2;
