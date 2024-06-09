231566393be27a8f93f80cc9ffaa8e94dd7c2c6d
Agustin <almartinez1@mail.austral.edu.ar>
2024-06-08 22:42:43 -0300

    css improvements

--- src/main/front/gamelib/src/Components/TheLibrary/Banner.jsx
+++ src/main/front/gamelib/src/Components/TheLibrary/Banner.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import {Link} from "react-router-dom";
+import React from 'react';
+import { Link } from "react-router-dom";
 
-function Banner({gameBanner}) {
-
-    return(
-        <div className='relative bg-black p-0.5 rounded-xl'>
+function Banner({ gameBanner }) {
+    return (
+        <div className='relative bg-black p-0.5 rounded-xl shadow-lg'>
             <Link to={'/videogame/' + gameBanner.id}>
-                <div className={'absolute bottom-0 p-6 bg-gradient-to-t from-slate-900 to-transparent w-full'}>
-                    <h2 className={'text-[40px] text-white font-bold py-1'}>{gameBanner.name}</h2>
-                    <button className={'bg-black text-white px-2 p-1 rounded-md py-0.5'}>Check it out!</button>
+                <div className={'absolute bottom-0 p-6 bg-gradient-to-t from-slate-900 to-transparent w-full rounded-b-xl'}>
+                    <h2 className={'text-[40px] text-white font-schibsted py-1'}>{gameBanner.name}</h2>
+                    <button className={'bg-orange-500 hover:bg-orange-600 text-white px-4 py-2 rounded-md transition-all duration-300'}>
+                        Check it out!
+                    </button>
                 </div>
-                <img src={gameBanner.background_image} className='md:w-full h-[500px] object-cover rounded-xl' alt={""}/>
+                <img src={gameBanner.background_image} className='md:w-full h-[500px] object-cover rounded-xl' alt={gameBanner.name} />
             </Link>
         </div>
-    )
+    );
 }
+
 export default Banner;
