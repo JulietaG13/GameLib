f08c6536deeda0149b81c36e411785a79ee81067
Agustin <almartinez1@mail.austral.edu.ar>
2024-06-08 22:41:29 -0300

    fonts added

--- src/main/front/gamelib/tailwind.config.js
+++ src/main/front/gamelib/tailwind.config.js
@@ -5,7 +5,26 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'Helvetica', 'Arial', 'sans-serif'],
+        title: ['Bebas Neue', 'cursive'],
+        anton: ['Anton', 'sans-serif'],
+        oswald: ['Oswald', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+        proxima: ['Proxima Nova', 'sans-serif'],
+        futura: ['Futura PT', 'sans-serif'],
+        avenir: ['Avenir', 'sans-serif'],
+        helvetica: ['Helvetica Neue', 'sans-serif'],
+        spartan: ['League Spartan', 'sans-serif'],
+        raleway: ['Raleway', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+        pressstart: ['Press Start 2P', 'cursive'],
+        exo: ['Exo', 'sans-serif'],
+        lobster: ['Lobster', 'cursive'],
+        schibsted: ['Schibsted Grotesk', 'sans-serif'], // Añadir esta línea
+      },
+    },
   },
   plugins: [],
 }
