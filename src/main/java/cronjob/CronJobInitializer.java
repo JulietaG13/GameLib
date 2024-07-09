aa50f37d43ac9ab9b848e2bf6618bb339e47d816
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-07-08 23:04:09 -0300

    Implemented a (Cron)Job that will be responsible for searching releasing games and notify each user who is subscribed to the respective games. Creates a CronJobInitializer that will take care of the Jobs scheduling.

---
+++ src/main/java/cronjob/CronJobInitializer.java
@@ -0,0 +1,51 @@
+package cronjob;
+
+import cronjob.jobs.NotifyReleasingGames;
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+import javax.persistence.EntityManagerFactory;
+
+public class CronJobInitializer {
+    private EntityManagerFactory factory;
+    private static CronJobInitializer instance;
+
+    public static CronJobInitializer getInstance(EntityManagerFactory factory) {
+        if (instance == null) {
+            instance = new CronJobInitializer(factory);
+        }
+        return instance;
+    }
+
+    CronJobInitializer(EntityManagerFactory factory) {
+        this.factory = factory;
+    }
+
+    public void initializeJobs() {
+        try {
+            SchedulerFactory schedulerFactory = new StdSchedulerFactory();
+            Scheduler scheduler = schedulerFactory.getScheduler();
+
+            scheduleNotifyReleasingGames(scheduler);
+
+            scheduler.start();
+        } catch (SchedulerException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void scheduleNotifyReleasingGames(Scheduler scheduler) throws SchedulerException {
+        JobDetail jobDetail = JobBuilder.newJob(NotifyReleasingGames.class)
+            .withIdentity("notifyReleasingGamesJob", "group1")
+            .usingJobData(new JobDataMap(java.util.Collections.singletonMap("entityManagerFactory", this.factory)))
+            .build();
+
+        Trigger trigger = TriggerBuilder.newTrigger()
+            .withIdentity("notifyReleasingGamesTrigger", "group1")
+            .withSchedule(CronScheduleBuilder.cronSchedule("0 0 0 * * ?"))
+            .build();
+        // Execute cronjob every 2 minutes: "0 */2 * * * ?"
+        // Execute cronjob every start of day: "0 0 0 * * ?"
+
+        scheduler.scheduleJob(jobDetail, trigger);
+    }
+}
---
+++ src/main/java/cronjob/jobs/NotifyReleasingGames.java
@@ -0,0 +1,74 @@
+package cronjob.jobs;
+
+import model.Game;
+import model.News;
+import model.User;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import repositories.GameRepository;
+import repositories.NewsRepository;
+import repositories.UserRepository;
+import services.NewsService;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public class NotifyReleasingGames implements Job {
+    @Override
+    public void execute(JobExecutionContext context) {
+        System.out.println("Executing NotifyReleasingGames job at " + LocalDate.now());
+
+        EntityManagerFactory factory = (EntityManagerFactory) context.getJobDetail().getJobDataMap().get("entityManagerFactory");
+        EntityManager entityManager = factory.createEntityManager();
+
+        // Get realising games.
+        LocalDate today = LocalDate.now();
+        List<Game> games = getTodayGames(entityManager, today);
+
+        if (games.isEmpty()) {
+            return;
+        }
+
+        // Get bot user
+        User bot = getBot(entityManager);
+
+        // Notify release from each game to users
+        NewsRepository newsRepository = new NewsRepository(entityManager);
+        games.forEach(game -> {
+            News releasingNew = createReleasingNews(game, bot, newsRepository);
+            NewsService.notifyUsers(releasingNew, entityManager);
+        });
+
+        entityManager.close();
+        System.out.println("Finishing NotifyReleasingGames job at " + LocalDate.now());
+    }
+
+    private List<Game> getTodayGames(EntityManager entityManager, LocalDate today) {
+        GameRepository gameRepository = new GameRepository(entityManager);
+        return gameRepository.findByDate(today);
+    }
+
+    private User getBot(EntityManager entityManager) {
+        String username = "bot";
+        UserRepository userRepository = new UserRepository(entityManager);
+        Optional<User> user = userRepository.findByUsername(username);
+
+        if (user.isEmpty()) {
+            throw new RuntimeException("Bot user not found");
+        }
+
+        return user.get();
+    }
+
+    private News createReleasingNews(Game game, User bot, NewsRepository newsRepository) {
+        String title = "New game released!";
+        String description = "The game " + game.getName() + " is releasing today! Don't miss it!";
+        News news = new News(title, description, game, bot);
+
+        newsRepository.persist(news);
+
+        return news;
+    }
+}
